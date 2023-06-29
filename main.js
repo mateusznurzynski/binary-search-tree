21570df0ed09368ca95950d6c725e6a72a6eec34
Mateusz Nurzyński <mateonurzynski@gmail.com>
2023-06-29 15:22:07 +0200

    Create a function that removes duplicates from an array

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
+const removeDuplicates = (array) => {
+  const lookupTable = {};
+  const cleanArray = array.filter((element) => {
+    if (lookupTable[element]) {
+      return false;
+    } else {
+      lookupTable[element] = true;
+      return true;
+    }
+  });
+  return cleanArray;
+};
+
+const buildTree = (array) => {
+  const cleanArray = removeDuplicates(array);
+};
+
 const Node = (data = null, leftChild = null, rightChild = null) => {
   return {
     data,
     leftChild,
     rightChild,
+  };
+};
+
+const Tree = (array) => {
+  return {
+    root: buildTree(array),
   };
 };
