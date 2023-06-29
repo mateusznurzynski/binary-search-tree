b0e91915cfa1e977c870e6ed47ea4429fb8f9ff4
Mateusz Nurzyński <mateonurzynski@gmail.com>
2023-06-29 15:48:54 +0200

    Create a function that turns arrays into balanced trees

--- main.js
+++ main.js
@@ -46,8 +46,27 @@ const removeDuplicates = (array) => {
   return cleanArray;
 };
 
-const buildTree = (array) => {
+const buildTree = (sortedArray, start = 0, end = sortedArray.length - 1) => {
+  if (end < start) {
+    return null;
+  }
+
+  const middle = parseInt((start + end) / 2);
+
+  const treeNode = Node(
+    sortedArray[middle],
+    buildTree(sortedArray, start, middle - 1),
+    buildTree(sortedArray, middle + 1, end)
+  );
+
+  return treeNode;
+};
+
+const initTree = (array) => {
   const cleanArray = removeDuplicates(array);
+  const sortedArray = mergeSort(cleanArray);
+
+  return buildTree(sortedArray);
 };
 
 const Node = (data = null, leftChild = null, rightChild = null) => {
@@ -60,6 +79,22 @@ const Node = (data = null, leftChild = null, rightChild = null) => {
 
 const Tree = (array) => {
   return {
-    root: buildTree(array),
+    root: initTree(array),
   };
 };
+
+const printTree = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.rightChild !== null) {
+    printTree(node.rightChild, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.leftChild !== null) {
+    printTree(node.leftChild, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
+const testTree = Tree([3, 2, 1, 5, 4, 6, 8, 7, 7, 9]);
+printTree(testTree.root);
